17f9000a1e811faf9c8c5b3519fec4a9ef7816a3
VetreX1337dev <gojda7693@gmail.com>
2026-02-18 18:18:06 +0300

    Update Mm2AimTrainer.lua

--- Mm2AimTrainer.lua
+++ Mm2AimTrainer.lua
@@ -1,1 +1,2 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VetreX1337dev/Scripts/refs/heads/main/esp.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VetreX1337dev/Scripts/refs/heads/main/Aimtraineracript.lua"))()
