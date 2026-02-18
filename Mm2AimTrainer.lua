0f9adc20c9c7f243d8ebc868db7f7e12841f399b
VetreX1337dev <gojda7693@gmail.com>
2026-02-18 18:31:41 +0300

    Delete Mm2AimTrainer.lua

--- Mm2AimTrainer.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/VetreX1337dev/Scripts/refs/heads/main/esp.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/VetreX1337dev/Scripts/refs/heads/main/Aimtraineracript.lua"))()
